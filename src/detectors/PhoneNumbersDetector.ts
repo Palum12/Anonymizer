4bfba93177c0aad19994ac77a0d9d1a98849b0b7
Maksymilian <xoil123@gmail.com>
2019-10-28 14:23:46 +0100

    finished phone detector, small tweaks to main regex

--- src/detectors/PhoneNumbersDetector.ts
+++ src/detectors/PhoneNumbersDetector.ts
@@ -1,29 +1,50 @@
-import Detector from './Detector';
+import {Detector}  from './Detector';
 
 export class PhoneNumbersDetector implements Detector {
     
     detectMatchingWords(words: string[]): string[] {
-        // throw new Error("Method not implemented.");
-        let phoneNumbers = [];
+        const phoneNumbers = [];
         for(let i = 0; i < words.length; i++) {
             let currentWord = words[i];
-            if (!this.isNumber(currentWord)) {
-                if(this.hasAreaCode(currentWord)) {
-                    let restOfNumber = currentWord.substr(3);
-                    // if(restOfNumber.length === 9 &&  this.isNumber(restOfNumber))
-                }
-                continue;
+            let result;
+            if(this.hasAreaCode(currentWord)) {
+                result = this.tryFindPhoneNumber([currentWord.substr(3)].concat(words.slice(i + 1, i + 3)));
+            } else {
+                result = this.tryFindPhoneNumber(words.slice(i, i + 3));
             }
-
+            if(result) {
+                phoneNumbers.push(result);
+            }
         }
         return phoneNumbers;
     }
 
+    private tryFindPhoneNumber(words: string[]): string {
+        const firstItem = words[0];
+        if(this.isNumber(firstItem)) {
+            if (firstItem.length == 9) {
+                return firstItem;
+            }
+            if (firstItem.length == 3 && this.isNumber(words[1]) && this.isNumber(words[2])) {
+                return words.slice(0, 3).join(' ');
+            }
+        } else if(firstItem && firstItem.split('-').length === 3) { // remove dashes and try again
+            if(this.tryFindPhoneNumber(firstItem.split('-'))) {
+                return firstItem;
+            }
+        }
+        return null;
+    }
+
     private isNumber(valueToCheck: string): boolean {
-        return !isNaN(parseInt(valueToCheck));
+        if(valueToCheck) {
+            return !isNaN(Number(valueToCheck));
+        }
+        return false;
     }
 
     private hasAreaCode(valueToCheck: string): boolean {
         return valueToCheck.substr(0, 3) === '+48';
     }
+
 }
