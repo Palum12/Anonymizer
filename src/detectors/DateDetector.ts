4003e72d412c962bb0dae67442d0f2377e68295e
Jakub Nadolny <matryl1@me.com>
2019-11-07 12:52:28 +0100

    Added stripe regex

--- src/detectors/DateDetector.ts
+++ src/detectors/DateDetector.ts
@@ -2,11 +2,12 @@ import {Detector} from "./Detector";
 
 export class DateDetector implements Detector {
     detectMatchingWords(words: string[]): string[] {
-        return words.filter(word => this.validateDate(word)); 
+        return words.filter(word => this.validateSimpleDate(word));
     }
 
-    private validateDate(word: string): boolean {
-        var re = /^ (0 ? [1 - 9] | 1[0 - 2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
-        return re.test(word);
+    private validateSimpleDate(word: string): boolean {
+        var dashRegex = /^ (0 ? [1 - 9] | 1[0 - 2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
+        var stripeRegex = /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/
+        return dashRegex.test(word) || lineRegex.test(word);
     }
 }
