b0c25be31a9d46c47f237521f1fe5fdcec8a9c9d
Jakub Nadolny <matryl1@me.com>
2019-11-07 12:56:28 +0100

    Fixed wrong variable name

--- src/detectors/DateDetector.ts
+++ src/detectors/DateDetector.ts
@@ -8,6 +8,6 @@ export class DateDetector implements Detector {
     private validateSimpleDate(word: string): boolean {
         var dashRegex = /^ (0 ? [1 - 9] | 1[0 - 2])[\/](0?[1-9]|[12]\d|3[01])[\/](19|20)\d{2}$/;
         var stripeRegex = /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/
-        return dashRegex.test(word) || lineRegex.test(word);
+        return dashRegex.test(word) || stripeRegex.test(word);
     }
 }
