080b85fe0783074f91c77b123c8ceea11d8ab61e
Maksymilian <xoil123@gmail.com>
2019-11-14 14:42:08 +0100

    dummy locality and postal codes detectors

---
+++ src/detectors/LocalityDetector.ts
@@ -0,0 +1,7 @@
+import { Detector } from './Detector';
+
+export class LocalityDetector implements Detector {
+    detectMatchingWords(words: string[]): string[] {
+        throw new Error("Method not implemented.");
+    }
+}
---
+++ src/detectors/PostalCodeDetector.ts
@@ -0,0 +1,7 @@
+import { Detector } from './Detector';
+
+export class PostalCodeDetector implements Detector {
+    detectMatchingWords(words: string[]): string[] {
+        throw new Error("Method not implemented.");
+    }
+}
