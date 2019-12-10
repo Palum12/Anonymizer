debf12cc27cc4cae12625e17d0af32eed126bea3
Maksymilian <xoil123@gmail.com>
2019-12-10 21:22:31 +0100

    standard regex

--- src/anonymizers/RegexAnonymizer.ts
+++ src/anonymizers/RegexAnonymizer.ts
@@ -5,22 +5,17 @@ import { AnonymizerDto } from "../../src/models/AnonymizerDto";
 export class RegexAnonymizer extends Anonymizer {
 
     protected parseText(text: AnonymizerDto) {
-        // todo
-        // let result = "/";
-        // const characters = text.split('');
-        // // here you can use this.regexRange now
-        // for (let character of characters) {
-        //     if (/[A-Z]/g.test(character)) {
-        //         result += "[A-Z]";
-        //     } else if (/[a-z]/g.test(character)) {
-        //         result += "[a-z]";
-        //     } else if (/[0-9]/g.test(character)) {
-        //         result += '[0-9]';
-        //     } else {
-        //         result += '.';
-        //     }
-        // }
-        // result += '/g';
-        // return result;
+        let result = "/";
+        const characters = text.originalText.split('');
+        for (let character of characters) {
+            if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/g.test(character)) {
+                result += `[\\${character}]`;
+                
+            } else {
+                result += `[${character}]`;
+            }
+        }
+        result += '/g';
+        text.anonymizedText = result;
     }
 }
